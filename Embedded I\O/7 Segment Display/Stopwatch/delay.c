64dd0bcb66d6a5945e2f38ec927a07c641476f42
Amit Sharma <95172045+amitsharma-codes@users.noreply.github.com>
2022-01-23 10:46:37 +0530

    Add files via upload

---
+++ Embedded I\O/7 Segment Display/Stopwatch/delay.c
@@ -0,0 +1,12 @@
+/*void delay_ms(unsigned int itime);
+void main()
+{
+	delay_ms(1000);
+}*/
+void delay_ms(unsigned int itime)
+{
+	unsigned int i,j;
+	for(i=0;i<itime;i++)
+	for(j=0;j<122;j++);
+	return;
+}
---
+++ Embedded I\O/7 Segment Display/Stopwatch/min.sec.c
@@ -0,0 +1,54 @@
+#include<reg51.h>
+#include"delay.h"
+#define seg_cat P2
+void disp_secs(unsigned int n);
+void disp_mins(unsigned int n);
+sbit CA1 = P3^0;
+sbit CA2 = P3^1;
+sbit CA3 = P3^2;
+sbit CA4 = P3^3;
+code char a[10]={0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90};
+void main()
+{
+	unsigned int i,j,k;
+	while(1)
+	{
+		for(i=0;i<60;i++)
+		{
+			for(j=0;j<60;j++)
+			{
+			for(k=0;k<100;k++)
+				{
+				disp_mins(i);
+			  disp_secs(j);
+				}
+			}
+		}
+	}		
+}
+void disp_secs(unsigned int n)
+{
+	seg_cat = a[n%10];
+	CA4=1;
+	delay_ms(2);
+	CA4=0;
+	
+	seg_cat = a[(n/10)%10];
+	CA3=1;
+	delay_ms(2);
+	CA3=0;
+}
+void disp_mins(unsigned int n)
+{
+	seg_cat = a[n%10] + 0x80;
+	//seg_cat|= 0x80;       //For adding DOT
+	CA2=1;
+	delay_ms(2);
+	CA2=0;
+	//seg_cat&= ~(0x80);   //For removing DOT on other segment
+	
+	seg_cat = a[(n/10)%10];
+	CA1=1;
+	delay_ms(2);
+	CA1=0;
+}
